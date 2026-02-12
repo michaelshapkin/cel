6cca67676606b05f86e095543e5be1ff448b7736
Michael Shapkin <riga.balticzone@gmail.com>
2026-02-12 11:39:22 +0200

    feat(lab): add exploit laboratory environment and documentation

---
+++ Exploit Laboratory/data/contracts/bedrock_defi/0x01e9161d1621466eb086651fd514d3efb8c3752e/VaultWithoutNative/interfaces/ISupplyFeeder.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+interface ISupplyFeeder {
+    /**
+     * @dev Calculate the current total supply of assets for 'token'.
+     */
+    function totalSupply(address token) external view returns(uint256);
+}
+
+interface IUniBTCSupplyFeeder {
+    function totalTokenSupply() external view returns (uint256);
+}
